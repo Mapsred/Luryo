48f75807363733d328a3ec805e9ec0832e4ea00d
uslaure <laure-ashley.graimont@supinternet.fr>
2016-07-07 12:15:54 +0200

    Configuration of FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             //Bundles
             new AppBundle\AppBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
