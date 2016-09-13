349d37715325603c2a2a1981e8a9ea2d644dfdb1
maps_red <maps_red@yahoo.fr>
2016-09-13 11:08:44 +0200

    Add Timestampable on Travel Entity

--- src/AppBundle/Entity/Travel.php
+++ src/AppBundle/Entity/Travel.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model as ORMbehaviors;
 
 /**
  * Travel
@@ -12,6 +13,8 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Travel
 {
+    use ORMbehaviors\Timestampable\Timestampable;
+
     /**
      * @var int
      *
