e69009145a9582fa7c27f2043cb28842c1caa0d8
maps_red <maps_red@yahoo.fr>
2016-09-22 10:37:53 +0200

    Add AdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             //Bundles
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
+            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
