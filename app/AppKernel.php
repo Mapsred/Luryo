f51b18af8a42a95f5ece0d4f20f2dc8b8f0bcb00
Francois <maps_red@yahoo.fr>
2016-10-11 00:20:21 +0200

    Remove adminbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,7 +35,6 @@ class AppKernel extends Kernel
             //Bundles
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
-            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
