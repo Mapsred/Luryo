f08564c1c39f3ce5e64222a31b55643689acfbe2
maps_red <maps_red@yahoo.fr>
2017-02-04 20:12:58 +0100

    Update field to nullable true

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -41,7 +41,7 @@ class User extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(name="sexe", type="string", length=255)
+     * @ORM\Column(name="sexe", type="string", length=255, nullable=true)
      */
     private $sexe;
 
